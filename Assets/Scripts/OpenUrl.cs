d92407ddc23fe2f7b87e9659967f13b2fe643380
mrosales <eric.mrosales@udgvirtual.udg.mx>
2025-02-03 17:58:22 -0600

    adding folders

---
+++ Assets/Scripts/OpenUrl.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OpenLink1()
+    {
+    	Application.OpenURL("https://www.google.com");
+    }
+    
+    public void OpenLink2()
+    {
+    	Application.OpenURL("https://www.google.com");
+    }
+    
+    public void OpenLink3()
+    {
+    	Application.OpenURL("https://www.google.com");
+    }
+    
+    public void OpenLink4()
+    {
+    	Application.OpenURL("https://www.google.com");
+    }
+    
+    public void OpenLink5()
+    {
+    	Application.OpenURL("https://www.google.com");
+    }
+    
+    public void OpenLink6()
+    {
+    	Application.OpenURL("https://www.google.com");
+    }
+}
